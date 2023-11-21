42c5a3fabedc4e0624856fb4034a491ff919bd2e
linaeveeee <137800080+linaeveeee@users.noreply.github.com>
2023-11-21 20:18:25 +0200

    homework09.1

---
+++ src/js/02-timer.js
@@ -0,0 +1,77 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+  inputDate: document.querySelector('input[type="text"]'),
+  startBtn: document.querySelector('button[data-start]'),
+  timerDays: document.querySelector('[data-days]'),
+  timerHours: document.querySelector('[data-hours]'),
+  timerMinutes: document.querySelector('[data-minutes]'),
+  timerSeconds: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.disabled = true;
+refs.startBtn.addEventListener('click', handleTimerStart);
+let userDate = 0;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userDate = selectedDates[0].getTime();
+    checkCorrectDate(userDate);
+  },
+};
+
+flatpickr(inputDate, options);
+
+function checkCorrectDate(date) {
+  const targetTime = deadlineTime(date);
+  if (targetTime <= 0) {
+    return Notiflix.Notify.failure('Please choose a date in the future');
+  }
+
+  refs.startBtn.disabled = false;
+}
+
+function deadlineTime(date) {
+  const currentDate = new Date().getTime();
+  const countDownTime = userDate - currentDate;
+  return countDownTime;
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function handleTimerStart() {
+  const inervalTimer = setInterval(() => {
+    let timeLeft = deadlineTime(userDate);
+
+    if (timeLeft <= 1000) {
+      clearInterval(inervalTimer);
+    }
+
+    function convertMs(ms) {
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+
+      const days = Math.floor(ms / day);
+      const hours = Math.floor((ms % day) / hour);
+      const minutes = Math.floor(((ms % day) % hour) / minute);
+      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+      timerDays.textContent = addLeadingZero(days);
+      timerHours.textContent = addLeadingZero(hours);
+      timerMinutes.textContent = addLeadingZero(minutes);
+      timerSeconds.textContent = addLeadingZero(seconds);
+    }
+
+    convertMs(timeLeft);
+  }, 1000);
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,55 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const refs = {
+  inputDelay: document.querySelector('[name="delay"]'),
+  inputStep: document.querySelector('[name="step"]'),
+  inputAmount: document.querySelector('[name="amount"]'),
+  promiseForm: document.querySelector('.form'),
+  promiseBtn: document.querySelector('button'),
+};
+
+refs.promiseForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  refs.promiseBtn.disabled = true;
+
+  let delay = Number(refs.inputDelay.value);
+  let step = Number(refs.inputStep.value);
+  let amount = Number(refs.inputAmount.value);
+
+  let totalDelay = 0;
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+
+    totalDelay += step;
   }
+  promiseForm.reset();
+  setTimeout(() => {
+    refs.promiseBtn.disabled = false;
+  }, delay);
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
